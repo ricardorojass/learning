1a4b762401ea8c837be11f6d7148c1cdd18f1db8
Ricardo Rojas <richi@Ricardos-MacBook-Pro.local>
2020-08-06 03:11:34 -0500

    Finish Linked List

--- DataStructures/LinkedList/index.ts
+++ DataStructures/LinkedList/index.ts
@@ -21,7 +21,7 @@ class LinkedList {
   }
 
   insertFirst(data: number | string) {
-    this.head = new NodeClass(data, this.head)
+    this.insertAt(data, 0)
   }
 
   size() {
@@ -36,21 +36,22 @@ class LinkedList {
   }
 
   getFirst() {
-    return this.head
+    return this.getAt(0)
   }
 
   getLast() {
-    if (!this.head) {
-      return null
-    }
+    // if (!this.head) {
+    //   return null
+    // }
 
-    let node = this.head
-    while (node) {
-      if (!node.next) {
-        return node
-      }
-      node = node.next
-    }
+    // let node = this.head
+    // while (node) {
+    //   if (!node.next) {
+    //     return node
+    //   }
+    //   node = node.next
+    // }
+    return this.getAt(this.size() - 1)
   }
 
   clear() {
@@ -61,7 +62,88 @@ class LinkedList {
     if (!this.head) {
       return
     }
+
     this.head = this.head.next
+  }
+
+  removeLast() {
+    if (!this.head) {
+      return
+    }
+
+    if (!this.head.next) {
+      this.head = null
+      return
+    }
+
+    let previous = this.head
+    let node = this.head.next
+    while (node.next) {
+      previous = node
+      node = node.next
+    }
+    previous.next = null
+  }
+
+  insertLast(data: number | string) {
+    const last = this.getLast()
+    if (last) {
+      // There are nodes
+      last.next = new NodeClass(data)
+    } else {
+      // List is empty
+      this.head = new NodeClass(data)
+    }
+  }
+
+  getAt(index: number) {
+    if (!this.head) {
+      return null
+    }
+    let counter = 0
+    let node = this.head
+    while (node) {
+      if (counter === index) {
+        return node
+      }
+
+      counter++
+      node = node.next
+    }
+    return null
+  }
+
+  removeAt(index: number) {
+    if (!this.head) {
+      return
+    }
+
+    if (index === 0) {
+      this.head = this.head.next
+      return
+    }
+
+    let previous = this.getAt(index - 1)
+    if (!previous || !previous.next) {
+      return
+    }
+    previous.next = previous.next.next
+  }
+
+  insertAt(data: string | number, index: number) {
+    if (!this.head) {
+      this.head = new NodeClass(data)
+      return
+    }
+
+    if (index === 0) {
+      this.head = new NodeClass(data, this.head)
+      return
+    }
+
+    const previous = this.getAt(index - 1) || this.getLast()
+    const node = new NodeClass(data, previous.next)
+    previous.next = node
   }
 }
 
@@ -87,5 +169,14 @@ console.log(
 console.log(
   list.removeFirst()
 )
+
+console.log(
+  list.getAt(0)
+)
+
+
+
+
+
 
 
