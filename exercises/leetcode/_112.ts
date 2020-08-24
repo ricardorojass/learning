7bb8384c424bbf54ab59e23bdd10eb33a055f799
Ricardo Rojas <richi@Ricardos-MacBook-Pro.local>
2020-08-23 22:11:20 -0500

    Add greatestSumPath

--- exercises/leetcode/_112.ts
+++ exercises/leetcode/_112.ts
@@ -3,6 +3,13 @@
 // path such that adding up all the values
 // along the path equals the given sum.
 
+//              5
+//             / \
+//            4   8
+//           /   / \
+//          11  13  4
+//         /  \      \
+//        7    2      1
 function hasPathSum(root: TreeNode | null, sum: number): boolean {
   if (root === null) return false
 
@@ -13,6 +20,25 @@ function hasPathSum(root: TreeNode | null, sum: number): boolean {
   }
 };
 
+
+
+// 5, 4, 7
+function greatestSumPath(root): number {
+
+  if (!root.left && !root.right) {
+    return root.val
+  } else {
+    const left = greatestSumPath(root.left)
+    const right = greatestSumPath(root.right)
+    if (left > right) {
+      return left + root.val
+    } {
+      return right + root.val
+  }
+
+
+
+}
 
 console.log(
   hasPathSum([5,4,8,11,null,13,4,7,2,null,null,null,1], 22),
