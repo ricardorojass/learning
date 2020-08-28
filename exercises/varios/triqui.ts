b8fff43a6a67037582f93c9ed86e04c3bbabb9b0
ricardorojass <reddrum7@gmail.com>
2020-08-28 00:17:01 -0500

    Improve Triqui exercise now recieve a matrix with dimension NxN

--- exercises/varios/triqui.ts
+++ exercises/varios/triqui.ts
@@ -1,74 +1,92 @@
 function triqui(board: string[][]) {
-  if (board.length && board[0].length && board[1].length && board[2].length) {
-    // Verificar las columnas
-    if (board[0][0] !== '-') {
-      // Columna 1 Fila 1
-      if (board[0][0] === board[1][0] && board[0][0] === board[2][0]) {
-        return true
-      }
-      if (board[0][0] === board[0][1] && board[0][0] === board[0][2]) {
-        return true
-      }
-      // Diag Arriba izq der
-      if (board[0][0] === board[1][1] && board[0][0] === board[2][2]) {
-        return true
-      }
+  let result = []
+  // filas
+  for (let i = 0; i < board.length; i++) {
+    const char = board[i][0]
+    if (char !== '-') {
+      result.push(isThereT(char, board[i]))
     }
-
-    if (board[0][1] !== '-') {
-      // Columna 2
-      if (board[0][1] === board[1][1] && board[0][1] === board[2][1]) {
-        return true
-      }
-    }
-
-    if (board[0][2] !== '-') {
-      // Columna 3
-      if (board[0][2] === board[1][2] && board[0][2] === board[2][2]) {
-        return true
-      }
-    }
-    // Verificar filas
-    if (board[1][0] !== '-') {
-      // Fila 2
-      if (board[1][0] === board[1][1] && board[1][0] === board[1][2]) {
-        return true
-      }
-    }
-    if (board[2][0] !== '-') {
-      // Fila 3
-      if (board[2][0] === board[2][1] && board[2][0] === board[2][2]) {
-        return true
-      }
-      // Diag Abajo izq der
-      if (board[2][0] === board[1][1] && board[2][0] === board[1][2]) {
-        return true
-      }
-    }
-    return false
-  } else {
-    return false
   }
+  // Columnas
+  for (let i = 0; i < board.length; i++) {
+    let columns = []
+    const char = board[0][i]
+
+    for (let j = 1; j < board[i].length; j++) {
+      columns.push(board[j][i])
+    }
+
+    if (char !== '-') {
+      result.push(isThereT(char, columns))
+    }
+  }
+
+  // Diagonal arriba izq der
+  let diagonales = []
+  for (let i = 1; i < board.length; i++) {
+    const char = board[0][i]
+    diagonales.push(board[i][i])
+    if (char !== '-') {
+      result.push(isThereT(char, diagonales))
+    }
+  }
+
+  // Diagonal arriba der izq
+  diagonales = []
+  for (let i = 0, j = board.length; i < board.length; i++) {
+    j--
+    diagonales.push(board[i][j])
+  }
+
+  const char = board[0][board.length - 1];
+  if (char !== '-') {
+    const match = isThereT(char, diagonales)
+    result.push(match)
+  }
+
+  function isThereT(value, arr) {
+    return arr.every(char => char === value)
+  }
+
+  return result.includes(true)
 }
+
 
 const board1 = [
   ['X', 'O', '-'],
   ['O', 'O', 'X'],
   ['-', 'O', 'X']
 ]
+
 const board2 = [
+  ['X', 'X', 'O'],
+  ['O', 'O', 'X'],
+  ['O', 'O', 'X']
+]
+const board3 = [
   ['-', '-', '-'],
   ['-', '-', '-'],
   ['-', '-', '-']
 ]
 
-const board3 = [
+const board4 = [
   ['-', '-', '-'],
   ['O', 'O', 'X'],
   ['-', 'O', 'X']
 ]
+
+const board5 = [
+  ['X', 'X', '-', 'X'],
+  ['O', 'O', 'X', 'O'],
+  ['-', 'X', 'X', '-'],
+  ['X', 'O', 'X', 'X']
+]
+
 console.log(
   triqui(board1)
+)
+console.log(
+  triqui(board5)
 )
 console.log(
   triqui(board2)
