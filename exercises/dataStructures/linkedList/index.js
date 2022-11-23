5c872e248f507206e3f28b1ebea5dcca0b55a9a8
ricardorojass <reddrum7@gmail.com>
2022-11-22 19:08:19 -0500

    add new methods to Linked list

--- exercises/dataStructures/linkedList/index.js
+++ exercises/dataStructures/linkedList/index.js
@@ -18,14 +18,13 @@ class LinkedList {
   }
 
   // Insert last node
-
   insertLast(data) {
-    let node = new Node(data);
+    let newNode = new Node(data);
     let current;
 
     // If empty, make head
     if (!this.head) {
-      this.head = node;
+      this.head = newNode;
     } else {
       current = this.head;
 
@@ -33,21 +32,93 @@ class LinkedList {
         current = current.next;
       }
 
-      current.next = node;
+      current.next = newNode;
     }
 
     this.size++;
   }
+
   // Insert at index
+  insertAt(data, index) {
+    // if index is out of range
+    if (index > 0 && index > this.size) {
+      return;
+    }
+
+    // if first index
+    if (index === 0) {
+      this.insertFirst(data);
+      return;
+    }
+
+    // traverse linked list
+    const node = new Node(data);
+    let current, previous;
+
+    current = this.head;
+    let currentIndex = 0;
+
+    while (currentIndex < index) {
+      previous = current; // Node before index
+      currentIndex++;
+      current = current.next; // Node after index
+    }
+
+    node.next = current;
+    previous.next = node;
+
+    this.size++;
+  }
+
 
   // Get at index
+  getAt(index) {
+    let current = this.head;
+    let currentIndex = 0;
+
+    while (current) {
+      if (currentIndex === index) {
+        console.log(current.data);
+      }
+      currentIndex++;
+      current = current.next;
+    }
+
+    return null;
+  }
 
   // Remove at index
+  removeAt(index) {
+    if (index > 0 && index > this.size) {
+      return;
+    }
+
+    let current = this.head;
+    let previous;
+    let currentIndex = 0;
+
+    // Remove first
+    if (index === 0) {
+      this.head = current.next
+    } else {
+      while(currentIndex < index) {
+        currentIndex++;
+        previous = current;
+        current = current.next;
+      }
+
+      previous.next = current.next;
+    }
+    this.size--;
+  }
 
   // Clear list
+  clearList() {
+    this.head = null;
+    this.size = 0;
+  }
 
   // Remove duplicates
-
   removeDups() {
     const set = new Set()
 
@@ -81,14 +152,10 @@ class LinkedList {
 const ll = new LinkedList();
 
 ll.insertFirst(100);
-ll.insertFirst(100);
 ll.insertFirst(200);
-ll.insertFirst(200);
-ll.insertFirst(300);
 ll.insertFirst(300);
 ll.insertLast(400);
-ll.removeDups()
 
-console.log(ll)
-
+ll.removeAt(3)
 ll.printListData()
+// ll.getAt(3);
