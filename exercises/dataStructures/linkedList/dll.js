c913bed024b23a6a8b23ac8ea03ba0c7b1f4a9e8
ricardorojass <reddrum7@gmail.com>
2022-11-30 22:36:05 -0500

    add double linked list

---
+++ exercises/dataStructures/linkedList/dll.js
@@ -0,0 +1,134 @@
+class Node {
+  constructor(value, next = null, prev = null) {
+    this.value = value;
+    this.next = next;
+    this.prev = prev;
+  }
+}
+
+class DLL {
+  constructor() {
+    this.head = null;
+  }
+
+  // Constant time
+  insertFirst(val) {
+    let node = new Node(val);
+    node.next = this.head;
+    node.prev = null;
+    if (this.head) {
+      this.head.prev = node;
+    }
+    this.head = node;
+  }
+
+  // O(n)
+  insertLast(val) {
+    let node = new Node(val);
+    let last = this.head;
+
+    node.next = null;
+
+    // If empty, make head
+    if (!this.head) {
+      node.prev = null;
+      this.head = node;
+      return;
+    }
+
+    while (last.next) {
+      last = last.next;
+    }
+    last.next = node;
+    node.prev = last;
+
+  }
+
+  insertAfter(after, val) {
+    let p = this.find(after);
+
+    if (!p) {
+      console.log('does not exist');
+      return;
+    }
+
+    let node = new Node(val);
+    node.next = p.next;
+    p.next = node;
+    node.prev = p;
+    if (node.next) {
+      node.next.prev = node;
+    }
+  }
+
+  deleteFirst() {
+    let val = this.head.value;
+    if (!this.head) {
+      console.log('zero nodes');
+      return;
+    }
+    this.head = this.head.next;
+    this.head.next = this.head.next;
+    this.head.prev = null;
+    return val;
+  }
+
+  deleteLast() {
+    // let val = this.head.value;
+    let last = this.head;
+    let prev = null;
+
+    while (last.next) {
+      last = last.next;
+      prev = last.prev;
+    }
+    last.next = null;
+    last.prev = prev;
+    return last;
+  }
+
+  find(val) {
+    let node = this.head;
+    while (node) {
+      if (node.value === val) {
+        return node;
+      }
+      node = node.next;
+    }
+    return null;
+  }
+
+  display() {
+    let node = this.head;
+    let last = null;
+    while(node) {
+      console.log(node.value + ' -> ');
+      last = node;
+      node = node.next;
+    }
+    console.log('END');
+    console.log(' ');
+
+    console.log('Print in reverse');
+    while (last) {
+      console.log(last.value + ' -> ');
+      last = last.prev;
+    }
+    console.log(' ');
+  }
+}
+
+const list = new DLL();
+
+list.insertFirst(3);
+list.insertFirst(2);
+list.insertFirst(8);
+list.insertFirst(17);
+list.insertLast(99);
+list.insertAfter(99, 65);
+// list.deleteFirst();
+list.deleteLast();
+// list.insertAt(100, 3);
+// console.log(list.removeAt(2))
+// console.log(list)
+list.display()
