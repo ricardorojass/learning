e592e89152b087edec0be3cd499e4378a506a6a6
ricardorojass <reddrum7@gmail.com>
2023-03-27 19:41:13 -0500

    Add non_constructible_change ex

---
+++ exercises/random/non_constructible_change.rb
@@ -0,0 +1,16 @@
+def non_constructible_change(coins)
+  coins.sort()
+
+  current_change_created = 0
+  for coin in coins
+    if coin > current_change_created + 1
+      return current_change_created + 1
+    end
+
+    current_change_created += coin
+  end
+
+  return current_change_created + 1
+end
+
+p non_constructible_change([1, 1, 2, 3, 5, 7, 22])
