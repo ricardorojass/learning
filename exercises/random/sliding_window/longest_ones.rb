19d21d62b133754921cfc8c5d0f4ee6f5aabb3bd
ricardorojass <reddrum7@gmail.com>
2023-06-05 18:24:49 -0500

    Add longest_ones exercise

---
+++ exercises/random/sliding_window/longest_ones.rb
@@ -0,0 +1,28 @@
+# @param {Integer[]} nums
+# @param {Integer} k
+# @return {Integer}
+def longest_ones(nums, k)
+  left, right, curr, ans = 0, 0, 0, 0
+
+  (0...nums.length).each do |right|
+
+    if (nums[right] == 0)
+      curr += 1
+    end
+
+    while (curr > k)
+      if (nums[left] == 0)
+        curr -= 1
+      end
+      left += 1
+    end
+
+    ans = [ans, right - left + 1].max
+
+  end
+
+  return ans
+
+end
+
+p longest_ones([1,1,1,0,0,0,1,1,1,1,0], 2)
