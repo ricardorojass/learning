dcace4f4d5971357ee72f076e5c3658e9292ad70
ricardorojass <reddrum7@gmail.com>
2023-06-09 21:56:49 -0500

    Refactor random exercise

--- exercises/random/abbreviate_two_word.rb
+++ exercises/random/abbreviate_two_word.rb
@@ -1,6 +1,6 @@
 # https://www.codewars.com/kata/57eadb7ecd143f4c9c0000a3/train/ruby
 def abbrev_name(name)
-  name.scan(/\w+/).map(&:chr).map(&:upcase).join('.')
+  name.upcase.split.map(&:chr).join('.')
 end
 
 p abbrev_name("ricardo, rojas")
